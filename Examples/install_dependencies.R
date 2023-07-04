7aa692a41b71b450a6f2268b8a86cd9047beee55
Shawn P. Serbin <sserbin@bnl.gov>
2023-07-04 12:57:05 -0400

    Cleanup and re-org. Added Examples/install_dependencies.R

---
+++ Examples/install_dependencies.R
@@ -0,0 +1,18 @@
+####################################################################################################
+# Install dependencies
+#
+# Note: to run source the script file
+#
+# last updated: 2023-06-30 by Shawn P. Serbin <sserbin@bnl.gov>
+####################################################################################################
+
+
+#--------------------------------------------------------------------------------------------------#
+req.packages <- c("devtools","dplyr","reshape2","here","ggplot2","gridExtra",
+                  "phenopix","zoo","matrixStats","outliers")
+new.packages <- req.packages[!(req.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, dependencies=TRUE)
+#--------------------------------------------------------------------------------------------------#
+
+# Restart R session to load new packages
+.rs.restartR()       # Restart R session
